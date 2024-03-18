f5e3386f81b0af1255633260df5b942064dc8895
Osman Koç <osman.koc@kocdigital.com>
2024-03-18 18:53:53 +0300

    chore: comment unused line

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def generate():
     if request.method == 'POST':
         font_amiko_bold = 'assets/fonts/Amiko-Bold.ttf'
         font_amiko_regular = 'assets/fonts/Amiko-Regular.ttf'
-        font_amiko_semibold = 'assets/fonts/Amiko-SemiBold.ttf'
+        # font_amiko_semibold = 'assets/fonts/Amiko-SemiBold.ttf'
 
         photo = request.files['photo']
         name = request.form['name']
