93157ea3890de28bd35c89581ac5355c23b29f43
Osman Koç <osman.koc@kocdigital.com>
2024-03-18 13:14:57 +0300

    feat: add multiple selection

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, request, send_file
+from flask import Flask, render_template, request, jsonify
 from PIL import Image, ImageDraw, ImageFont
 import os
+import base64
+import io
 
 app = Flask(__name__)
 
@@ -14,28 +16,79 @@ def generate():
         photo = request.files['photo']
         name = request.form['name']
         slogan = request.form['slogan']
-
-        # Kullanıcının yüklediği fotoğrafı kaydet
-        photo.save(os.path.join('static', photo.filename))
+        afis_type = request.form['type']
+        belediye = request.form['belediye']
+        
+        # Afiş tipine göre görseli seç
+        if afis_type == 'akp':
+            afis_image = 'afis_akp.jpeg'
+            name_color = 'white'
+            slogan_color = 'black'
+            belediye_color = 'black'
+            name_fontSize = 86
+            slogan_fontSize = 26
+            belediye_fontSize = 56
+            name_position = (390, 250)
+            slogan_position = (390, 380)
+            belediye_position = (390, 450)
+        elif afis_type == 'chp':
+            afis_image = 'afis_chp.jpeg'
+            name_color = 'red'
+            slogan_color = 'black'
+            belediye_color = 'white'
+            name_fontSize = 86
+            slogan_fontSize = 26
+            belediye_fontSize = 56
+            name_position = (390, 250)
+            slogan_position = (390, 380)
+            belediye_position = (390, 450)
+        elif afis_type == 'mhp':
+            afis_image = 'afis_mhp.jpeg'
+            name_color = 'red'
+            slogan_color = 'black'
+            belediye_color = 'black'
+            name_fontSize = 86
+            slogan_fontSize = 26
+            belediye_fontSize = 56
+            name_position = (390, 250)
+            slogan_position = (390, 380)
+            belediye_position = (390, 450)
+        elif afis_type == 'diger' or afis_type == 'muhtar':
+            afis_image = 'afis_diger.jpeg' if afis_type == 'diger' else 'afis_muhtar.jpeg'
+            name_color = 'red'
+            slogan_color = 'black'
+            belediye_color = 'white'
+            name_fontSize = 76
+            slogan_fontSize = 26
+            belediye_fontSize = 56
+            name_position = (390, 250)
+            slogan_position = (390, 380)
+            belediye_position = (380, 48)
 
         # Afiş oluştur
-        img = Image.open('static/afis.jpg')
+        img = Image.open(os.path.join('static', afis_image))
         draw = ImageDraw.Draw(img)
-        fontName = ImageFont.truetype('static/ARLRDBD.ttf', 86)
-        fontSlogan = ImageFont.truetype('static/ARLRDBD.ttf', 26)
+        fontName = ImageFont.truetype('static/ARLRDBD.ttf', name_fontSize)
+        fontSlogan = ImageFont.truetype('static/ARLRDBD.ttf', slogan_fontSize)
+        fontBelediye = ImageFont.truetype('static/ARLRDBD.ttf', belediye_fontSize)
 
         # Fotoğrafı afişe ekle
-        user_photo = Image.open(os.path.join('static', photo.filename))
+        user_photo = Image.open(photo)
         user_photo = user_photo.resize((327, 455))
         img.paste(user_photo, (20, 16))
 
-        # İsim ve sloganı afişe yaz
-        draw.text((390, 250), f'{name}', fill='black', font=fontName)
-        draw.text((390, 380), f'{slogan}', fill='black', font=fontSlogan)
+        # İsim, slogan ve belediye/mahalle ismi yaz
+        draw.text(name_position, f'{name}', fill=name_color, font=fontName)
+        draw.text(slogan_position, f'{slogan}', fill=slogan_color, font=fontSlogan)
+        draw.text(belediye_position, f'{belediye}', fill=belediye_color, font=fontBelediye)
 
-        # Oluşan afişi kaydet ve indirme linkini gönder
-        img.save(os.path.join('static', 'generated_afis.png'))
-        return send_file(os.path.join('static', 'generated_afis.png'), as_attachment=True)
+        # Oluşan afişi base64 formatında döndürme
+        img_io = io.BytesIO()
+        img.save(img_io, format='PNG')
+        img_io.seek(0)
+        img_base64 = base64.b64encode(img_io.getvalue()).decode()
+
+        return jsonify({"data": img_base64})
 
 if __name__ == '__main__':
     app.run(debug=True)
